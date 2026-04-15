de856ec7380d0c8530cfeedbce5b6b77f26454fc
shaun825 <shaun@shaunnel.me>
2026-04-15 11:10:51 +0200

    Update and rename next.config.ts to next.config.js

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["pdf-parse", "@prisma/client"]
-  }
-};
-
-export default nextConfig;
